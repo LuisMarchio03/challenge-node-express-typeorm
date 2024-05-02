e2bb14c3a5ed4123383e39d12d299431a87d7465
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2024-05-02 17:12:26 -0300

    feat(clients): Add CreateDTO and UpdateDTO

---
+++ src/modules/dtos/client-create-dto.ts
@@ -0,0 +1,8 @@
+export interface IClientCreateDto {
+    tecl_id: number;
+    tecl_nome: string;
+    tecl_endereco: string;
+    tecl_cidade: string;
+    tecl_uf: string;
+    tecl_telefone: string
+}
---
+++ src/modules/dtos/client-update-dto.ts
@@ -0,0 +1,7 @@
+export interface IClientUpdateDto {
+    tecl_nome: string;
+    tecl_endereco: string;
+    tecl_cidade: string;
+    tecl_uf: string;
+    tecl_telefone: string
+}
