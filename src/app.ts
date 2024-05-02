3503bbd6927acd19c6bfa13f2d17f57b5c97ca36
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2024-05-02 17:09:58 -0300

    feat: Set up initial Node.js project with Express

---
+++ src/app.ts
@@ -0,0 +1,42 @@
+import "reflect-metadata";
+import "dotenv/config";
+import cors from "cors";
+
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
+
+
+// import { associateRoutes } from "./module/http/controllers/routes"
+import { connectToDatabase } from "./database";
+
+const app = express();
+
+const main = async () => {
+  await connectToDatabase();
+};
+
+main().catch(err => {
+  console.error(err);
+  process.exit(1);
+});
+
+app.use(express.json());
+app.use(cors());
+// app.use(associateRoutes);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      return response.status(400).json({
+        error: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "Error",
+      message: "Internal Server Error",
+    });
+  }
+);
+
+export { app };
