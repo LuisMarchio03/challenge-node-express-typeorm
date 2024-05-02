276096d3357dc821a35835f22f4e0822ccb6e5f5
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2024-05-02 17:50:15 -0300

    refactor(client): change name files

---
+++ src/modules/use-cases/clients/create-client.spec.ts
@@ -0,0 +1,41 @@
+import { describe, it, expect, beforeEach } from 'vitest'
+import { CreateClientUseCase } from './create-client'
+import { InMemoryClientRepository } from '../../repositories/in-memory/in-memory-clients-repository'
+import { IClientsRepository } from '../../repositories/client-repository'
+    
+let sut: CreateClientUseCase
+let clientRepository: IClientsRepository
+
+describe("Create client use-case", () => {
+    beforeEach(() => {
+        clientRepository = new InMemoryClientRepository()
+        sut = new CreateClientUseCase(clientRepository)
+    })
+
+    it('should be able to create client', async () => {
+        const { tecl_nome, tecl_id } = await sut.execute({
+            tecl_id: 1,
+            tecl_cidade: "Any city",
+            tecl_endereco: "Any address",
+            tecl_nome: "Any name",
+            tecl_telefone: "00999999999",
+            tecl_uf: "Any UF"
+        })
+
+        expect(tecl_nome).toEqual(expect.any(String))
+        expect(tecl_id).toEqual(expect.any(Number))
+    })
+
+    it('should be able to not create Associate if it already exists', async () => {
+        const client = await sut.execute({
+            tecl_id: 1,
+            tecl_cidade: "Any city",
+            tecl_endereco: "Any address",
+            tecl_nome: "Any name",
+            tecl_telefone: "00999999999",
+            tecl_uf: "Any UF"
+        })
+
+        expect(client).toBeTruthy()
+    })
+})
---
+++ src/modules/use-cases/clients/update-client.spec.ts
@@ -0,0 +1,40 @@
+import { describe, it, expect, beforeEach } from 'vitest'
+
+import { UpdateClientUseCase } from './update-client'
+import { InMemoryClientRepository } from '../../repositories/in-memory/in-memory-clients-repository'
+    
+let sut: UpdateClientUseCase
+let clientRepository: InMemoryClientRepository
+
+describe('Update Client Use Case', () => {
+  beforeEach(() => {
+    clientRepository = new InMemoryClientRepository()
+    sut = new UpdateClientUseCase(clientRepository)
+  })
+
+  it('should be able to update Client', async () => {
+    const created = await clientRepository.create({
+        tecl_id: 1,
+        tecl_cidade: "Any city",
+        tecl_endereco: "Any address",
+        tecl_nome: "Any name",
+        tecl_telefone: "00999999999",
+        tecl_uf: "Any UF"
+    })
+
+    await sut.execute({
+        tecl_id: created.TECL_ID,
+        tecl_cidade: "Any city Update",
+        tecl_endereco: "Any address Update",
+        tecl_nome: "Any name Update",
+        tecl_telefone: "00999999999",
+        tecl_uf: "Any UF"
+    })
+
+    const client = await clientRepository.listClient(created.TECL_ID)
+
+    expect(client.TECL_NOME).toEqual('Any name Update')
+    expect(client.TECL_ENDERECO).toEqual("Any address Update")
+    expect(client.TECL_CIDADE).toEqual('Any city Update')
+  })
+})
