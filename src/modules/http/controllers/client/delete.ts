1b1f8df9e97824374193a79ed37f211dadeb0e92
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2024-05-03 09:20:51 -0300

    refactor: response delete client

--- src/modules/http/controllers/client/delete.ts
+++ src/modules/http/controllers/client/delete.ts
@@ -1,15 +1,30 @@
 import { Request, Response } from 'express'
 import { makeDeleteClientUseCase } from '../../../use-cases/factories/make-delete-client-use-case' 
 
+interface CustomRequest extends Request {
+  token?: string; 
+}
 export class DeleteController {
-  async handler(req: Request, res: Response) {
+  async handler(req: CustomRequest, res: Response) {
     try {
       const deleteClientUseCase = makeDeleteClientUseCase()
       await deleteClientUseCase.execute(parseInt(req?.params?.tecl_id))
     
-      return res.status(200).send()
+      return res.status(200).json({
+        success: true,
+        message: "Apagado com sucesso!",
+        data: null,
+        error: null,
+        token: req.token
+      })
     } catch (err: any) {
-      return res.status(400).json({ error: err.message })
+      return res.status(400).json({ 
+        success: false,
+        message: err.message,
+        data: null,
+        error: true,
+        token: req.token
+      })
     }
   }
 
