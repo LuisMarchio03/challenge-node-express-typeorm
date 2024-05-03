6cbe95c7c83144acbe9caa36eddb7c6f581b23f5
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2024-05-03 09:21:06 -0300

    refactor: response list clients

--- src/modules/http/controllers/client/list-clients.ts
+++ src/modules/http/controllers/client/list-clients.ts
@@ -1,15 +1,28 @@
 import { Request, Response } from 'express'
 import { makeListClientsUseCase } from '../../../use-cases/factories/make-list-clients-use-case' 
+import { CustomRequest } from 'utils/types'
 
-export class ListController {
-  async handler(req: Request, res: Response) {
+export class ListAllController {
+  async handler(req: CustomRequest, res: Response) {
     try {
       const listClientsUseCase = makeListClientsUseCase()
       const clients = await listClientsUseCase.execute()
     
-      return res.status(200).json(clients)
+      return res.status(200).json({
+        success: true,
+        message: null,
+        data: clients,
+        error: null,
+        token: req.token
+      })
     } catch (err: any) {
-      return res.status(400).json({ error: err.message })
+      return res.status(400).json({ 
+        success: false,
+        message: err.message ,
+        data: null,
+        error: true,
+        token: req.token
+      })
     }
   }
 
